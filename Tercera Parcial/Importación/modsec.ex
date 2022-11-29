a7431fffba8e949bb20ee3f08943860de5515e8b
Hector-Amaya <111697789+Hector-Amaya@users.noreply.github.com>
2022-11-28 21:20:17 -0600

    Create modsec.ex

---
+++ Tercera Parcial/Importación/modsec.ex
@@ -0,0 +1,10 @@
+#Héctor Eduardo Amaya González 3B
+
+#Creamos el Módulo a importar modsec.ex
+#-Escribimos el siguiente código:
+
+defmodule ModuloImportado do
+  def funcion_importada(msg) do
+    IO.puts(msg)
+  end
+end
